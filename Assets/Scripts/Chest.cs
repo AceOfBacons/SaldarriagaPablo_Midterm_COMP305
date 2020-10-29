f14114566472b0a8ac6b05c819c1937a7caf5b45
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2020-10-28 23:49:56 -0400

    Finished level layout and and camera confiner

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public bool isOpen = false;
+    public Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void openChest()
+    {
+        if(Input.GetKeyDown("e"))
+        {
+            anim.SetBool("isOpen", true);
+        }
+
+
+    }
+}
+
+
+
+
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("player in range");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInRange = false;
+        Debug.Log("player now not in range");
+    }
+}
