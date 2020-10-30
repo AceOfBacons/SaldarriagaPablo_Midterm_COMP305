3cab110b42cfbd32fea045f37426f4dea18f6974
Pablo Saldarriaga <psaldarr@my.centennialcollege.ca>
2020-10-29 23:00:39 -0400

    Star Spawner

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -6,18 +6,44 @@ public class Chest : MonoBehaviour
 {
     public bool isOpen = false;
     public Animator anim;
+    public GameObject star;
+    public Transform spawnPoint;
 
-    public void Start()
+    public float spawnRate;
+    private bool isInRange = false;
+    private float spawnCounter = 2;
+
+    public void Update()
     {
-        anim = GetComponent<Animator>();
-    }
-    public void openChest()
-    {
-        if(Input.GetKeyDown("e"))
+        if(isInRange && Input.GetKeyDown(KeyCode.E))
         {
             anim.SetBool("isOpen", true);
+            isOpen = true;
+
+        }
+        if (isOpen)
+        {
+            spawnCounter -= Time.deltaTime;
+            if (spawnCounter <= 0)
+            {
+                spawnCounter = spawnRate;
+                Instantiate(star, spawnPoint.transform.position, spawnPoint.transform.rotation);
+            }
         }
 
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isInRange = false;
 
     }
 }
--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
-    public float upForce = 1f;
-    public float sideForce = 0f;
-    private Rigidbody2D rb;
+    //public float upForce = 1f;
+    //public float sideForce = 0f;
+    //private Rigidbody2D rb;
+
+    [SerializeField] private float speed = 7.5f;
+    private Vector3 direction;
+
     void Start()
     {
-        float xForce = Random.Range(-sideForce, sideForce);
-        float yForce = Random.Range(upForce / 2f, upForce);
+        //float xForce = Random.Range(-sideForce, sideForce);
+        //float yForce = Random.Range(upForce / 2f, upForce);
 
-        Vector2 force = new Vector2(xForce, yForce);
+        //Vector2 force = new Vector2(xForce, yForce);
 
-        rb.GetComponent<Rigidbody2D>();
-        rb.velocity = force;
+        //rb.GetComponent<Rigidbody2D>();
+        //rb.velocity = force;
+
+        direction = Vector3.up;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += direction * speed * Time.deltaTime;
     }
+
 }
